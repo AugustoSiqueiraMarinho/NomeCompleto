4e68312faca6fdb97cecef28e1f5ff8497858674
Augusto S. <augustosiqueira000@gmail.com>
2025-09-24 20:12:32 -0300

    Exercício Finalizado

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Digite seu nome: ");
+string nome = Console.ReadLine()!;
+
+Console.Write("Digite seu sobrenome: ");
+string sobrenome = Console.ReadLine()!;
+
+Console.Write($"Boa noite, {nome} {sobrenome}, Prazer em conhecer você!");
